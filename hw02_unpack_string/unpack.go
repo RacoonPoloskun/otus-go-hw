bd9abbf170ef1014306e49b9aecc8960c11e63a3
Pavel <pasha-voronin@yandex.ru>
2023-02-26 04:47:30 +0300

    hw02_unpack_string / Домашнее задание №2 «Распаковка строки»

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,42 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var i int
+	var builder strings.Builder
+
+	explodedString := []rune(str)
+	explodedStrLength := len(explodedString)
+
+	for i < explodedStrLength {
+		char := explodedString[i]
+
+		if unicode.IsDigit(char) {
+			return "", ErrInvalidString
+		}
+
+		if (i+1 < explodedStrLength) && (unicode.IsDigit(explodedString[i+1])) {
+			i++
+			stringNum, err := strconv.Atoi(string(explodedString[i]))
+
+			if err != nil {
+				return "", ErrInvalidString
+			}
+
+			builder.WriteString(strings.Repeat(string(char), stringNum))
+		} else {
+			builder.WriteRune(char)
+		}
+
+		i++
+	}
+
+	return builder.String(), nil
 }
