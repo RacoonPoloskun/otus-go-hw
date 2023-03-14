e4180dc1c9efea16b2be4505d726ddbfd9110aef
Pavel <pasha-voronin@yandex.ru>
2023-03-14 22:27:56 +0300

    hw03_frequency_analysis / lint fix

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -18,20 +18,21 @@ func newAnalyzer() *analyzer {
 	return &analyzer{}
 }
 
-func (anl *analyzer) analyze(text string, count int) []string {
-	anl.parse(text)
+func (a *analyzer) analyze(text string, count int) []string {
+	a.parse(text)
 
-	if len(anl.words) == 0 {
+	if len(a.words) == 0 {
 		return nil
 	}
 
-	anl.sort()
+	a.sort()
 
-	return anl.splice(count)
+	return a.splice(count)
 }
 
-func (anl *analyzer) parse(text string) {
-	reg, _ := regexp.Compile(`\S+`)
+func (a *analyzer) parse(text string) {
+	reg := regexp.MustCompile(`\S+`)
+
 	words := map[string]int{}
 
 	for _, word := range reg.FindAllString(text, -1) {
@@ -39,29 +40,29 @@ func (anl *analyzer) parse(text string) {
 	}
 
 	for text, count := range words {
-		anl.words = append(anl.words, word{text, count})
+		a.words = append(a.words, word{text, count})
 	}
 }
 
-func (anl *analyzer) sort() {
-	sort.Slice(anl.words, func(i, j int) bool {
-		if anl.words[i].count == anl.words[j].count {
-			return anl.words[i].text < anl.words[j].text
+func (a *analyzer) sort() {
+	sort.Slice(a.words, func(i, j int) bool {
+		if a.words[i].count == a.words[j].count {
+			return a.words[i].text < a.words[j].text
 		}
 
-		return anl.words[i].count > anl.words[j].count
+		return a.words[i].count > a.words[j].count
 	})
 }
 
-func (anl *analyzer) splice(count int) []string {
-	if count > len(anl.words) {
-		count = len(anl.words)
+func (a *analyzer) splice(count int) []string {
+	if count > len(a.words) {
+		count = len(a.words)
 	}
 
 	result := make([]string, count)
 
-	for i, _ := range result {
-		result[i] = anl.words[i].text
+	for i := range result {
+		result[i] = a.words[i].text
 	}
 
 	return result
