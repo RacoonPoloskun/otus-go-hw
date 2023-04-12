fe18a17a3957a477f734b2f1f6e0413d10aacb64
Pavel Voronin <pavel.voronin@pochtavip.com>
2023-04-12 20:26:40 +0300

    hw04_lru_cache / hw lru_cache

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,24 +1,101 @@
 package hw04lrucache
 
 type List interface {
-	Len() int
-	Front() *ListItem
-	Back() *ListItem
-	PushFront(v interface{}) *ListItem
-	PushBack(v interface{}) *ListItem
-	Remove(i *ListItem)
-	MoveToFront(i *ListItem)
+	Len() int                          // длина списка
+	Front() *ListItem                  // первый элемент списка
+	Back() *ListItem                   // последний элемент списка
+	PushFront(v interface{}) *ListItem // добавить значение в начало
+	PushBack(v interface{}) *ListItem  // добавить значение в конец
+	Remove(i *ListItem)                // удалить элемент
+	MoveToFront(i *ListItem)           // переместить элемент в начало
 }
 
 type ListItem struct {
-	Value interface{}
-	Next  *ListItem
-	Prev  *ListItem
+	Value interface{} // значение
+	Next  *ListItem   // следующий элемент
+	Prev  *ListItem   // предыдущий элемент
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := new(ListItem)
+	item.Value = v
+
+	if l.Len() > 0 {
+		item.Next = l.front
+		item.Next.Prev = item
+	} else {
+		l.back = item
+	}
+
+	l.front = item
+	l.len++
+
+	return item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := new(ListItem)
+	item.Value = v
+
+	if l.Len() > 0 {
+		item.Prev = l.back
+		item.Prev.Next = item
+	} else {
+		l.front = item
+	}
+
+	l.back = item
+	l.len++
+
+	return item
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Next == nil {
+		l.back = i.Prev
+	} else {
+		i.Next.Prev = i.Prev
+	}
+
+	if i.Prev == nil {
+		l.front = i.Next
+	} else {
+		i.Prev.Next = i.Next
+	}
+
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev == nil {
+		return
+	}
+
+	l.Remove(i)
+
+	i.Prev = nil
+	i.Next = l.front
+
+	l.front.Prev = i
+	l.front = i
+	l.len++
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len         int
+	front, back *ListItem
 }
 
 func NewList() List {
