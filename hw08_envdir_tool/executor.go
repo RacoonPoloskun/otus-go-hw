332caa74a328c09ced61e7af2cf5c970c680c712
Pavel Voronin <pavel.voronin@pochtavip.com>
2023-04-19 18:06:45 +0300

    hw08_envdir_tool / executor fix

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -17,35 +17,22 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
-
-	if err := handleEnvVars(env); err != nil {
-		return exitError
-	}
+	command.Env = buildEnv(env)
 
 	_ = command.Run()
 
 	return command.ProcessState.ExitCode()
 }
 
-func handleEnvVars(env Environment) error {
-	for envVar, val := range env {
-		if val.NeedRemove {
-			if err := os.Unsetenv(envVar); err != nil {
-				return err
-			}
-			break
+func buildEnv(env Environment) []string {
+	for key, env := range env {
+		if env.NeedRemove {
+			_ = os.Unsetenv(key)
+			continue
 		}
 
-		if _, ok := os.LookupEnv(envVar); ok {
-			if err := os.Unsetenv(envVar); err != nil {
-				return err
-			}
-		}
-
-		if err := os.Setenv(envVar, val.Value); err != nil {
-			return err
-		}
+		_ = os.Setenv(key, env.Value)
 	}
 
-	return nil
+	return os.Environ()
 }
